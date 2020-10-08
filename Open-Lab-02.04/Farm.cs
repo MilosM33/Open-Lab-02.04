8c683dcad8e95dc0c2fc43eb1d85e36e5c118bbf
MilosM33 <milosmarejka89@gmail.com>
2020-10-08 13:32:21 +0200

    init

--- Open-Lab-02.04/Farm.cs
+++ Open-Lab-02.04/Farm.cs
@@ -4,9 +4,7 @@ namespace Open_Lab_02._04
 {
     class Farm
     {
-        public int GetLegsCount(int chickens, int cows, int pigs)
-        {
-            throw new NotImplementedException();
-        }
+        public int GetLegsCount(int chickens, int cows, int pigs) => chickens * 2 + cows * 4 + pigs * 4;
+        
     }
 }
